fe98105d8f0c794d48ad749de2b5f8fb985c6742
aucrawford <adrian.crawford@sleepygiant.com>
2013-06-13 23:47:37 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScratchIme::Application.config.secret_token = 'd19aba8c96f14fa73f2da2bed9190030682ff4152e3bcfcb0d7274df33d6dd377d0241e38e0a67f9b081a26a41fc7e723846b7d5e2e896fd95ea760f5e402cfb'

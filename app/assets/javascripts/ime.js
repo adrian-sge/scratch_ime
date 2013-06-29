29dc5995a23390b640523f16b0da481bd3cfa6fd
John Goldsmith <john.goldsmith@sleepygiant.com>
2013-06-29 14:24:58 -0700

    Uncomment JS and just explicitly include needed JS

--- app/assets/javascripts/ime.js
+++ app/assets/javascripts/ime.js
@@ -1,162 +1,150 @@
-// var lines = [],
-//     index = 0,
-//     newIndex;
+(function ($, window, document) {
 
-// do {
-//   newIndex = text().indexof('\n', index);
-//   lines.push(text().substr(index, newIndex !== -1 ? newIndex - index + 1 : void 0));
-//   index = newIndex + 1;
-// } while (newIndex !== -1);
+  var
 
-// return lines;
+    // Set some defaults
+    defaults = {
+      debug : false,
+      debugTarget : false,
+      event : 'keyup'
+    },
 
-// (function ($, window, document) {
+    // Set the name of the plugin
+    pluginName = 'inputMethodEditor';
 
-//   var
+  /**
+   * Constructor
+   */
 
-//     // Set some defaults
-//     defaults = {
-//       debug : false,
-//       debugTarget : false,
-//       event : 'keyup'
-//     },
+  function InputMethodEditor (element, options) {
 
-//     // Set the name of the plugin
-//     pluginName = 'inputMethodEditor';
+    // Set the current DOM node being acted upon
+    this.element = element;
 
-//   /**
-//    * Constructor
-//    */
+    // Set the current jQuery object being acted upon
+    this.$element = $(element);
 
-//   function InputMethodEditor (element, options) {
+    // Set the current DOM node tag name being acted upon (e.g. "select" or "a")
+    if ( this.$element.prop('tagName') && typeof( this.$element.prop('tagName') ) == 'string' ) {
+      this.htmlTag = this.$element.prop('tagName').toLowerCase();
+    } else {
+      this.htmlTag = false;
+    }
 
-//     // Set the current DOM node being acted upon
-//     this.element = element;
+    // Set allowed events
+    this.allowedEvents = [
+      'keydown', 'keypress', 'keyup'
+    ];
 
-//     // Set the current jQuery object being acted upon
-//     this.$element = $(element);
+    // Set allowed keys
+    this.allowedKeyCodes = [
+      12, // clear
+      13, // enter
+      96, // Numpad 0
+      97, // Numpad 1
+      98, // Numpad 2
+      99, // Numpad 3
+      100, // Numpad 4
+      101, // Numpad 5
+      102, // Numpad 6
+      103, // Numpad 7
+      104, // Numpad 8
+      105, // Numpad 9
+      106, // Numpad *
+      107, // Numpad +
+      108, // Numpad 9
+      109, // Numpad -
+      110, // Numpad .
+      111, // Numpad /
+      187 // Numpad =
+    ];
 
-//     // Set the current DOM node tag name being acted upon (e.g. "select" or "a")
-//     if ( this.$element.prop('tagName') && typeof( this.$element.prop('tagName') ) == 'string' ) {
-//       this.htmlTag = this.$element.prop('tagName').toLowerCase();
-//     } else {
-//       this.htmlTag = false;
-//     }
+    // Merge the options into the defaults
+    this.options = $.extend( true, {}, defaults, options );
 
-//     // Set allowed events
-//     this.allowedEvents = [
-//       'keydown', 'keypress', 'keyup'
-//     ];
+    // Set a reference to the original, un-merged defaults
+    this._defaults = defaults;
 
-//     // Set allowed keys
-//     this.allowedKeyCodes = [
-//       12, // clear
-//       13, // enter
-//       96, // Numpad 0
-//       97, // Numpad 1
-//       98, // Numpad 2
-//       99, // Numpad 3
-//       100, // Numpad 4
-//       101, // Numpad 5
-//       102, // Numpad 6
-//       103, // Numpad 7
-//       104, // Numpad 8
-//       105, // Numpad 9
-//       106, // Numpad *
-//       107, // Numpad +
-//       108, // Numpad 9
-//       109, // Numpad -
-//       110, // Numpad .
-//       111, // Numpad /
-//       187 // Numpad =
-//     ];
+    // Set a reference to the name of the plugin
+    this._name = pluginName;
 
-//     // Merge the options into the defaults
-//     this.options = $.extend( true, {}, defaults, options );
+    // Away we go!
+    this.init();
 
-//     // Set a reference to the original, un-merged defaults
-//     this._defaults = defaults;
+  };
 
-//     // Set a reference to the name of the plugin
-//     this._name = pluginName;
+  InputMethodEditor.prototype.init = function () {
+    var self = this;
 
-//     // Away we go!
-//     this.init();
+    this.exec = function () {
+      this.checkValidBindEvents();
+    };
 
-//   };
+    this.checkValidBindEvents = function () {
+      if ( $.isArray( this.options.event ) ) {
+        var validEvents = [];
+        for ( var e in this.options.event ) {
+          if ( $.inArray( e, self.allowedEvents ) > -1 ) {
+            validEvents.push(e);
+          };
+        };
+        this.options.event = validEvents.join(' ');
+        this.bindEvents();
+      } else if ( $.inArray( this.options.event, this.allowedEvents ) > -1 ) {
+        this.bindEvents();
+      };
+    };
 
-//   InputMethodEditor.prototype.init = function () {
-//     var self = this;
+    this.log = function (message) {
+      if ( this.options.debug && typeof( this.options.debugTarget ) == 'string' && this.options.debugTarget != '' ) {
+        $( this.options.debugTarget ).append( message );
+        $( this.options.debugTarget ).scrollTop( $( this.options.debugTarget ).prop('scrollHeight') );
+        $( this.options.debugTarget ).append("<hr />")
+      };
+    };
 
-//     this.exec = function () {
-//       this.checkValidBindEvents();
-//     };
+    this.bindEvents = function () {
+      $(document).on( this.options.event, '#' + this.$element[0].id, function (event) {
+        self.log('Keycode = ' + event.which);
+        self.val = self.$element.val();
+        if ( $.inArray( event.which, self.allowedKeyCodes ) > -1 ) {
+          self.allowKey();
+        } else {
+          self.disallowKey();
+        };
+      });
+    };
 
-//     this.checkValidBindEvents = function () {
-//       if ( $.isArray( this.options.event ) ) {
-//         var validEvents = [];
-//         for ( var e in this.options.event ) {
-//           if ( $.inArray( e, self.allowedEvents ) > -1 ) {
-//             validEvents.push(e);
-//           };
-//         };
-//         this.options.event = validEvents.join(' ');
-//         this.bindEvents();
-//       } else if ( $.inArray( this.options.event, this.allowedEvents ) > -1 ) {
-//         this.bindEvents();
-//       };
-//     };
+    this.disallowKey = function () {
+      this.log('Disallowed key');
+      this.val = this.val.substring(0, this.val.length - 1);
+      this.$element.val( this.val );
+    };
 
-//     this.log = function (message) {
-//       if ( this.options.debug && typeof( this.options.debugTarget ) == 'string' && this.options.debugTarget != '' ) {
-//         $( this.options.debugTarget ).append( message );
-//         $( this.options.debugTarget ).scrollTop( $( this.options.debugTarget ).prop('scrollHeight') );
-//         $( this.options.debugTarget ).append("<hr />")
-//       };
-//     };
+    this.allowKey = function () {
+      this.log('Allowed key');
+    };
 
-//     this.bindEvents = function () {
-//       $(document).on( this.options.event, '#' + this.$element[0].id, function (event) {
-//         self.log('Keycode = ' + event.which);
-//         self.val = self.$element.val();
-//         if ( $.inArray( event.which, self.allowedKeyCodes ) > -1 ) {
-//           self.allowKey();
-//         } else {
-//           self.disallowKey();
-//         };
-//       });
-//     };
+    this.exec();
 
-//     this.disallowKey = function () {
-//       this.log('Disallowed key');
-//       this.val = this.val.substring(0, this.val.length - 1);
-//       this.$element.val( this.val );
-//     };
+  };
 
-//     this.allowKey = function () {
-//       this.log('Allowed key');
-//     };
+  /*
+   * Plugin wrapper
+   */
 
-//     this.exec();
+$.fn[pluginName] = function (options) {
 
-//   };
+    // For every matched selector found...
+    return this.each( function () {
 
-//   /*
-//    * Plugin wrapper
-//    */
+      // Set some jQuery data if it doesn't already exist
+      if ( !$.data( this, 'plugin_' + pluginName ) ) {
+        $.data( this, 'plugin_' + pluginName, new InputMethodEditor( this, options ) );
+      };
 
-// $.fn[pluginName] = function (options) {
+    });
 
-//     // For every matched selector found...
-//     return this.each( function () {
+  };
 
-//       // Set some jQuery data if it doesn't already exist
-//       if ( !$.data( this, 'plugin_' + pluginName ) ) {
-//         $.data( this, 'plugin_' + pluginName, new InputMethodEditor( this, options ) );
-//       };
-
-//     });
-
-//   };
-
-// })(jQuery, window, document);
+})(jQuery, window, document);
--- app/assets/javascripts/updater.js
+++ app/assets/javascripts/updater.js
@@ -1,38 +1,41 @@
-//   $(document).ready( function() {
-//   var textarea = $("#ime");
-//    textarea.onkeypress = function(evt) {
-//        var charCode = evt.which;
-//        var charStr = String.fromCharCode(charCode);
-//        console.log(charStr);
-//    };
+  $(document).ready( function() {
 
-//   // numpad 7 key detect and remap to 1
-//   $('#ime').onkeypress( function (e) {
-//     var value = $(this).val(),
-//         lastchar = value.substring(value.lenght -1, value.lenght);
+  var textarea = $("#ime");
 
-//     console.log(lastchar);
+  textarea.onkeypress = function(evt) {
+    var charCode = evt.which,
+        charStr = String.fromCharCode(charCode);
+    console.log(charStr);
+  };
 
-//     if (e.which == 103) {
-//       var output = lastchar.replace(/[7]/, "1");
-//       $(this).val(output);
-//     } else if (e.which == 104) {
-//       var output = lastchar.replace(/[8]/, "2");
-//       $(this).val(output);
-//     } else if (e.which == 105) {
-//       var output = lastchar.replace(/[9]/, "3");
-//       $(this).val(output);
-//     } else if (e.which == 97) {
-//       var output = lastchar.replace(/[1]/, "7");
-//       $(this).val(output);
-//     } else if (e.which == 98) {
-//       var output = lastchar.replace(/[2]/, "8");
-//       $(this).val(output);
-//     } else if (e.which == 99) {
-//       var output = lastchar.replace(/[3]/, "0");
-//       $(this).val(output);
-//     }
-//   }).keydown( function (e) {
-//      // e.preventDefault();
-//   });
-// });
+  // numpad 7 key detect and remap to 1
+  $('#ime').onkeypress( function (e) {
+    var value = $(this).val(),
+        lastchar = value.substring(value.length -1, value.length);
+
+    console.log(lastchar);
+
+    if (e.which == 103) {
+      var output = lastchar.replace(/[7]/, "1");
+      $(this).val(output);
+    } else if (e.which == 104) {
+      var output = lastchar.replace(/[8]/, "2");
+      $(this).val(output);
+    } else if (e.which == 105) {
+      var output = lastchar.replace(/[9]/, "3");
+      $(this).val(output);
+    } else if (e.which == 97) {
+      var output = lastchar.replace(/[1]/, "7");
+      $(this).val(output);
+    } else if (e.which == 98) {
+      var output = lastchar.replace(/[2]/, "8");
+      $(this).val(output);
+    } else if (e.which == 99) {
+      var output = lastchar.replace(/[3]/, "0");
+      $(this).val(output);
+    }
+  }).keydown( function (e) {
+     // e.preventDefault();
+  });
+
+});
